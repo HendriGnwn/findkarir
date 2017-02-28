4e2188226761828eb068147728768428f29e6fab
Hendri <hendri.gnw@gmail.com>
2017-02-28 17:19:41 +0700

    update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -37,6 +37,7 @@
 | in the URL cannot be matched to a valid route.
 |
 */
+$route['login/sign-in/(:num)'] = 'login/masukDulu/$1';
 
 $route['default_controller'] = "beranda/index";
 $route['404_override'] = 'error/index404';
