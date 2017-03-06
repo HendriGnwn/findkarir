30a4061b0cbfd816f4923561207373f008886fdb
Hendri <hendri.gnw@gmail.com>
2017-03-06 16:08:16 +0700

    add components authManager

--- config/console.php
+++ config/console.php
@@ -25,6 +25,9 @@ $config = [
             ],
         ],
         'db' => $db,
+		'authManager' => [
+            'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\DbManager'
+        ],
         'urlManager' => [
             'class' => 'yii\web\UrlManager',
             'scriptUrl' => $params['mainUrl'],
